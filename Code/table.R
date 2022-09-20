0237605fbe0f09992a9fe30b3cdfff91a702c343
MichaelMarcaccio <michael.marcaccio@uconn.edu>
2022-09-20 13:43:11 -0400

    commit #cleanup

--- Code/table.R
+++
@@ -1,8 +0,0 @@
-
-n <- 10
-dat <- data.frame(normal = rnorm(n),
-                  poisson = rpois(n, lambda = 4),
-                  gamma = rgamma(n, scale = 2, shape = 2))
-
-xtab <- table(dat, digits = c(0, 3, 0, 3))
-print(xtab, include.rownames = FALSE)
